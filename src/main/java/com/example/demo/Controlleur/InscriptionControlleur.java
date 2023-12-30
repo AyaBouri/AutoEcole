ed42d879a8c75c6b88c2b62034b162fbac817b1a
Aya Bouri <aya.bouri1@gmail.com>
2023-12-30 18:41:54 +0100

    src

--- src/main/java/com/example/demo/Controlleur/InscriptionControlleur.java
+++ src/main/java/com/example/demo/Controlleur/InscriptionControlleur.java
@@ -1,6 +1,8 @@
 package com.example.demo.Controlleur;
 import java.beans.PropertyEditorSupport;
 import java.time.LocalDate;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.demo.Model.User;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 @Controller
 @RequestMapping("/inscription")
@@ -39,5 +44,16 @@ public class InscriptionControlleur {
         }
         // Traitement d'inscription ici
         return "inscriptionSuccess";
+    }
+    @PostMapping("/inscription")
+    public String inscription(@Valid @ModelAttribute("user") User user,BindingResult bindingResult,HttpServletResponse response){
+        if(bindingResult.hasErrors()){
+            return "inscription";
+        }
+        String token=UUID.randomUUID().toString();
+        Cookie cookie=new Cookie("user_token",token);
+        cookie.setMaxAge(60 * 60 *24 * 30);//30 jours
+        response.addCookie(cookie);
+        return "redirect:/home";
     }
 }
