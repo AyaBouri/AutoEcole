6ad473f36b40d006202d7ce1fe14b28433e07556
Aya Bouri <aya.bouri1@gmail.com>
2023-12-30 12:41:03 +0100

    src

--- src/main/java/com/example/demo/Entity/Login.java
+++ src/main/java/com/example/demo/Entity/Login.java
@@ -17,5 +17,34 @@ public class Login {
     @Column(name = "password")
     private String password;
     @OneToMany(mappedBy = "userid")
-    private List<User> users;   
+    private List<User> users; 
+    //getter and setter
+    //login id
+    public Long getLoginId(){
+        return loginid;
+    }  
+    public void setLoginId(Long loginId){
+        this.loginid=loginId;
+    }
+    //email
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    //password
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    //user
+    public List<User> getUser(){
+        return users;
+    }
+    public void setUser(List<User> users){
+        this.users=users;
+    }
 }
