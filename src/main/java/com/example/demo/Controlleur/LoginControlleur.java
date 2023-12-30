1e62d5f966d9bdfed92483b74a3ec01c29e40527
Aya Bouri <aya.bouri1@gmail.com>
2023-12-30 15:57:44 +0100

    src

--- src/main/java/com/example/demo/Controlleur/LoginControlleur.java
+++ src/main/java/com/example/demo/Controlleur/LoginControlleur.java
@@ -2,6 +2,8 @@ package com.example.demo.Controlleur;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginControlleur {   
     @GetMapping("/login")
@@ -10,32 +12,25 @@ public class LoginControlleur {
         model.addAttribute("password", "");
         return "login";
     }
-}
-/*
     @PostMapping("/login")
-    public String login(
-            @RequestParam("email") String email,
-            @RequestParam("password") String password,
-            Model model
-    ) {
-        if (email.isEmpty() || password.isEmpty()) {
+    public String login(@RequestParam("email") String email,@RequestParam("password") String password,Model model){
+        if(email.isEmpty() || password.isEmpty()){
             model.addAttribute("error", "Email or password is empty");
             return "login";
         }
-
-        // Vérifiez l'email
-        if (!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")) {
-            model.addAttribute("error", "Email is invalid");
+        //verifier l'email
+        if(!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")){
+            model.addAttribute("error", "Email is Invalid");
             return "login";
         }
-
-        // Vérifiez le mot de passe
-        if (password.length() < 8) {
+        //Verifier password
+        if(password.length()<8){
             model.addAttribute("error", "Password must be at least 8 characters long");
             return "login";
         }
-
         // Si toutes les informations sont correctes, connectez l'utilisateur
-        return "redirect:/home";
+        else{
+            return "redirect:/home";
+        }
     }
- */
+}
