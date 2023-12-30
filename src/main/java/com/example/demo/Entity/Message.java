c5519fb327e474d11311e4bb6a94b4dcbb1d88bc
Aya Bouri <aya.bouri1@gmail.com>
2023-12-30 12:17:25 +0100

    src

--- src/main/java/com/example/demo/Entity/Message.java
+++ src/main/java/com/example/demo/Entity/Message.java
@@ -1,12 +1,48 @@
 package com.example.demo.Entity;
+import java.time.LocalDate;
+import com.example.demo.Model.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 @Entity
 @Table(name ="message")
 public class Message {  
     @Id
     @Column(name = "messageId")
-    private Long messageId; 
+    private Long messageId;
+    @Column(name = "libelle") 
+    private String libelle;
+    @Column(name = "Description")
+    private String description;
+    @Column(name = "date_envoie")
+    private LocalDate date_envoie;
+    @OneToOne(mappedBy = "userid")
+    private User user;
+    //getter and setters
+    public Long getMessageId(){
+        return messageId;
+    }
+    public void setMessageId(Long messageId){
+        this.messageId=messageId;
+    }
+    public String getLibelle(){
+        return libelle;
+    }
+    public void setLibelle(String Libelle){
+        this.libelle=Libelle;
+    }
+    public LocalDate getDateEnvoie(){
+        return date_envoie;
+    }
+    public void setDateEnvoie(LocalDate date_envoie){
+        this.date_envoie=date_envoie;
+    }
+    public User getUser(){
+        return user;
+    }
+    public void setUser(User user){
+        this.user=user;
+    }
 }
